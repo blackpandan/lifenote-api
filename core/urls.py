65c2860616f5b07fa035324bb39fce433bfc301f
blackpandan <faithgstc54@gmail.com>
2021-01-05 05:58:53 -0500

    added document route

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import get_all_todos, get_all_projects, modify_todo, modify_projects, get_user_details, register_user
+from .views import document, get_all_todos, get_all_projects, modify_todo, modify_projects, get_user_details, register_user
 
 #for rest_framework imports
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
+    path('', document),
     path("projects/all", get_all_projects),
     path("auth/token", obtain_auth_token),
     path("projects/modify/<int:id>", modify_projects),
