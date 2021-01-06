6f4ebff5fd9ed2753e4364f1df8c48b3e4dcc9d5
blackpandan <faithgstc54@gmail.com>
2021-01-05 20:52:16 -0500

    updated Token import route

--- core/signals.py
+++ core/signals.py
@@ -4,11 +4,11 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 # for rest_framework imports
-from rest_framework import Token
+from rest_framework.authtoken.models import Token
 
 
 # this is for automatic token creation
-@reciever(post_save, sender=settings.AUTH_USER_MODEL)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
