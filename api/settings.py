3f901f0abde7cd16a74dcac0a29d1abbb6849883
blackpandan <faithgstc54@gmail.com>
2021-01-05 22:59:53 -0500

    updated settings for postgres

--- api/settings.py
+++ api/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,6 +140,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfile'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+django_heroku.settings(local())
